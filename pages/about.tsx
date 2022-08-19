8119277683610791e8dbca3ddda2e043dd6401dc
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-19 12:27:27 -0400

    :sparkles: [ADD] support to TypeScripts

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <p>Esta es la pagina de About</p>
+    </>
+  );
+};
+
+export default About;
