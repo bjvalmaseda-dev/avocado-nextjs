60b805585fae44b216c4b61365993677191b6624
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-20 15:46:48 -0400

    :wheelchair: [ADD] currency format

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,27 +1,38 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
-import Link from 'next/link'
+import React from "react";
+import { Card } from "semantic-ui-react";
+import Link from "next/link";
 
 type ProductListProps = {
-  products: TProduct[]
-}
+  products: TProduct[];
+};
+
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 const mapProductsToCards = (products: TProduct[]) =>
-  products.map(({ name, id, price, image }) => (
-    <Link key={id} href="/product/[id]" as={`/product/${id}`} passHref>
-      <Card
-        as="a"
-        header={name}
-        image={image}
-        meta={<Card.Meta style={{ color: 'dimgray' }}>{price}</Card.Meta>}
-      />
-    </Link>
-  ))
+  products.map(({ name, id, price, image }) => {
+    return (
+      <Link key={id} href="/product/[id]" as={`/product/${id}`} passHref>
+        <Card
+          as="a"
+          header={name}
+          image={image}
+          meta={
+            <Card.Meta style={{ color: "dimgray" }}>
+              {intl.format(price)}
+            </Card.Meta>
+          }
+        />
+      </Link>
+    );
+  });
 
 const ProductList = ({ products }: ProductListProps) => (
   <Card.Group itemsPerRow={2} stackable>
     {mapProductsToCards(products)}
   </Card.Group>
-)
+);
 
-export default ProductList
+export default ProductList;
