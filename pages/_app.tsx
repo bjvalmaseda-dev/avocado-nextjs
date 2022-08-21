c6f15c461e75f8f6eb8bca24349ab85512f1c430
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-21 07:36:52 -0400

    :monocle_face: [ADD] metrics collector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import { AppProps } from 'next/app'
-import 'semantic-ui-css/semantic.min.css'
-import '../global.css'
+import { AppProps } from "next/app";
+import "semantic-ui-css/semantic.min.css";
+import "../global.css";
+import { NextWebVitalsMetric } from "next/app";
 
-import CartProvider from '@store/Cart'
+import CartProvider from "@store/Cart";
+
+export function reportWebVitals(metrics: NextWebVitalsMetric) {
+  console.log(metrics);
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
@@ -12,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <CartProvider>
       <Component {...pageProps} />
     </CartProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
